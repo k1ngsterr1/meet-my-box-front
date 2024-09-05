83ca631c37ed75edf8e617f190df1e133d614031
Erlan-bam <erlanzh.gg@gmail.com>
2024-09-05 21:10:02 +0500

    --BG LOGO

--- src/widgets/ui/screens/MainScreen/MainScreen.tsx
+++ src/widgets/ui/screens/MainScreen/MainScreen.tsx
@@ -2,6 +2,7 @@ import Button from "@shared/ui/Button/ui/button";
 import { Paragraph } from "@shared/ui/Paragraph/ui/paragraph";
 import group from "@assets/clients_group.png";
 import main_img from "@assets/main_img.png";
+import bg_logo from "@assets/bg_face.svg";
 
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { IconButton } from "@shared/ui/IconButton/ui/icon-button";
@@ -10,6 +11,11 @@ import styles from "./styles.module.scss";
 export const MainScreen = () => {
   return (
     <main className={styles.main_screen}>
+      <img
+        src={bg_logo.src}
+        alt="bg_logo"
+        className={styles.main_screen__logo}
+      />
       <h1 className={styles.main_screen__heading}>
         Cервис доставки посылок из Европы в Россию и страны СНГ
       </h1>
