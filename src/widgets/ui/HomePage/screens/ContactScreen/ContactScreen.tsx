3764e520347cd635ff936c26c343891d77417707
Erlan-bam <erlanzh.gg@gmail.com>
2024-09-07 21:02:36 +0500

    --WIDTH 86.6 ADDED

--- src/widgets/ui/HomePage/screens/ContactScreen/ContactScreen.tsx
+++ src/widgets/ui/HomePage/screens/ContactScreen/ContactScreen.tsx
@@ -6,25 +6,49 @@ import { ContactLink } from "@shared/ui/Link/Contact/contact-link";
 import contact_img from "@assets/Form Screen.png";
 export const ContactScreen = () => {
   return (
-    <div className={styles.contact}>
-      <h2 className={styles.contact__heading}>Свяжитесь с нами</h2>
-      <Paragraph margin="mt-4" width="w-3/4" isCentered={true}>
-        Вы можете связаться с нами через Telegram или WhatsApp, либо заполнить
-        форму и мы сами свяжемся с вами
-      </Paragraph>
-      <ContactForm />
-      <Button text="Отправить" buttonType="filled" margin="mt-10" />
-      <ContactLink
-        text="info@meetmybox.com"
-        link="mailto:info@meetmybox.com"
-        margin="mt-10"
-      />
-      <ContactLink
-        text="(720) 709-9413"
-        link="tel:+17207099413"
-        margin="mt-4"
-      />
-      <img src={contact_img.src} alt="form_img" className="mt-4 w-full" />
-    </div>
+    <>
+      <div className={styles.contact}>
+        <h2 className={styles.contact__heading}>Свяжитесь с нами</h2>
+        <Paragraph margin="mt-4" width="w-3/4" isCentered={true}>
+          Вы можете связаться с нами через Telegram или WhatsApp, либо заполнить
+          форму и мы сами свяжемся с вами
+        </Paragraph>
+        <ContactForm />
+        <Button text="Отправить" buttonType="filled" margin="mt-10" />
+        <ContactLink
+          text="info@meetmybox.com"
+          link="mailto:info@meetmybox.com"
+          margin="mt-10"
+        />
+        <ContactLink
+          text="(720) 709-9413"
+          link="tel:+17207099413"
+          margin="mt-4"
+        />
+        <img src={contact_img.src} alt="form_img" className="mt-4 w-full" />
+      </div>
+      <div className={styles.contact_pc}>
+        <div className={styles.contact_pc__left}>
+          <h2 className={styles.contact_pc__heading}>Свяжитесь с нами</h2>
+          <Paragraph margin="mt-4" width="w-3/4" isCentered={true}>
+            Вы можете связаться с нами через Telegram или WhatsApp, либо
+            заполнить форму и мы сами свяжемся с вами
+          </Paragraph>
+          <ContactForm />
+          <Button text="Отправить" buttonType="filled" margin="mt-10" />
+          <ContactLink
+            text="info@meetmybox.com"
+            link="mailto:info@meetmybox.com"
+            margin="mt-10"
+          />
+          <ContactLink
+            text="(720) 709-9413"
+            link="tel:+17207099413"
+            margin="mt-4"
+          />
+        </div>
+        <img src={contact_img.src} alt="form_img" className="mt-4 w-full" />
+      </div>
+    </>
   );
 };
