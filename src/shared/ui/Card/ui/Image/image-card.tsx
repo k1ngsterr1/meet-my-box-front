351c44a28206d5de900bb0f9f2de1d55695745c5
Erlan-bam <erlanzh.gg@gmail.com>
2024-09-07 15:55:07 +0500

    --PROCESS MOB FINISHED

--- src/shared/ui/Card/ui/Image/image-card.tsx
+++ src/shared/ui/Card/ui/Image/image-card.tsx
@@ -6,7 +6,7 @@ export const ImageCard: React.FC<IImageCardItem> = ({ img, description }) => {
   return (
     <div className={styles.item}>
       <img src={img.src} alt="image card" className={styles.item__img} />
-      <Paragraph isCentered margin="mt-4" width="w-2/3">
+      <Paragraph isCentered margin="mt-4" width="w-full">
         {description}
       </Paragraph>
     </div>
--- src/widgets/ui/HomePage/screens/ProcessScreen/ProcessScreen.tsx
+++ src/widgets/ui/HomePage/screens/ProcessScreen/ProcessScreen.tsx
@@ -1,13 +1,21 @@
-import { ProcessList } from "@shared/ui/Process/process-list";
+import { ProcessList, ProcessListPC } from "@shared/ui/Process/process-list";
 import { Fade } from "react-awesome-reveal";
 import styles from "./styles.module.scss";
 export const ProcessScreen = () => {
   return (
-    <div className={styles.process}>
-      <Fade>
-        <h2 className={styles.process__heading}>Процессы работы</h2>
-      </Fade>
-      <ProcessList />
-    </div>
+    <>
+      <div className={styles.process}>
+        <Fade>
+          <h2 className={styles.process__heading}>Процессы работы</h2>
+        </Fade>
+        <ProcessList />
+      </div>
+      <div className={styles.process_pc}>
+        <Fade>
+          <h2 className={styles.process_pc__heading}>Процессы работы</h2>
+        </Fade>
+        <ProcessListPC />
+      </div>
+    </>
   );
 };
