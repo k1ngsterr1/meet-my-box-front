e3e88afd91b0cdf18364111e1530a20f583268eb
Erlan-bam <erlanzh.gg@gmail.com>
2024-09-08 22:17:49 +0500

    --LOGIN PC AND NEW LAYOUT

--- src/features/ui/Header/header.tsx
+++ src/features/ui/Header/header.tsx
@@ -2,12 +2,27 @@ import logo from "@assets/logo_mob.svg";
 import { BurgerButton } from "@shared/ui/Burger/ui/burger-button";
 import Button from "@shared/ui/Button/ui/button";
 import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
 
 interface IHeader {
   isMobile?: boolean;
 }
 
 export const Header: React.FC<IHeader> = ({ isMobile }) => {
+  const [userEmail, setUserEmail] = useState(null);
+
+  useEffect(() => {
+    // Check for userData in localStorage
+    const userData = localStorage.getItem("userData");
+    if (userData) {
+      try {
+        const user = JSON.parse(userData);
+        setUserEmail(user.email); // Set the email if userData exists
+      } catch (error) {
+        console.error("Error parsing userData:", error);
+      }
+    }
+  }, []);
   return (
     <>
       <header className={styles.header}>
@@ -31,13 +46,17 @@ export const Header: React.FC<IHeader> = ({ isMobile }) => {
           <a className={styles.header_pc__nav__link} href="#contacts">
             Стоимость
           </a>
-          <Button
-            text="Войти"
-            buttonType="filled"
-            onClick={() => {
-              window.location.href = "/login";
-            }}
-          />
+          {userEmail ? (
+            <span className={styles.header_pc__email}>{userEmail}</span>
+          ) : (
+            <Button
+              text="Войти"
+              buttonType="filled"
+              onClick={() => {
+                window.location.href = "/login";
+              }}
+            />
+          )}
         </nav>
       </header>
     </>
--- src/widgets/ui/RegistrationPage/index.tsx
+++ src/widgets/ui/RegistrationPage/index.tsx
@@ -3,10 +3,17 @@ import bg_face from "@assets/bg_face.svg";
 import { RegistrationForm } from "@features/RegistrationForm";
 export const RegistrationPage = () => {
   return (
-    <div className={styles.register}>
-      <img src={bg_face.src} alt="" className={styles.register__img} />
-      <h2 className={styles.register__heading}>Вход в личный кабинет</h2>
-      <RegistrationForm />
-    </div>
+    <>
+      <div className={styles.register}>
+        <img src={bg_face.src} alt="" className={styles.register__img} />
+        <h2 className={styles.register__heading}>Вход в личный кабинет</h2>
+        <RegistrationForm />
+      </div>
+      <div className={styles.register_pc}>
+        <img src={bg_face.src} alt="" className={styles.register_pc__img} />
+        <h2 className={styles.register_pc__heading}>Вход в личный кабинет</h2>
+        <RegistrationForm />
+      </div>
+    </>
   );
 };
