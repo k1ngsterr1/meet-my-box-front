88a45ad0546bb904dd85348665832ddb63d37457
Erlan-bam <erlanzh.gg@gmail.com>
2024-09-09 19:38:17 +0500

    --FORM WORKING

--- src/shared/ui/Input/Calculate/calculate-input.tsx
+++ src/shared/ui/Input/Calculate/calculate-input.tsx
@@ -4,12 +4,14 @@ import type { ICalculateInput } from "@shared/lib/content/Input";
 export const CalculateInput: React.FC<ICalculateInput> = ({
   placeholder,
   margin,
+  ...rest
 }) => {
   return (
     <input
       type="text"
       placeholder={placeholder}
       className={`${styles.input} ${margin || ""}`}
+      {...rest}
     />
   );
 };
