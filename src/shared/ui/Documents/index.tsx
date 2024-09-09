a90b882b9e6c5e4a219403f0613e9c4dc0439771
Erlan-bam <erlanzh.gg@gmail.com>
2024-09-09 20:17:18 +0500

    --ADDED DOCS

---
+++ src/shared/ui/Documents/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import Button from "../Button/ui/button";
+interface Props {
+  onClick: () => void;
+}
+
+export const Documents: React.FC<Props> = ({ onClick }) => {
+  return (
+    <div className={styles.documents_pc}>
+      <p className={styles.documents_pc__p}>Вес максимум 1 кг</p>
+      <p className={styles.documents_pc__p}>Сумма трех сторон максимум 85 см</p>
+      <p className={styles.documents_pc__p}>
+        Самая длинная сторона максимум 50 см
+      </p>
+      <p className={styles.documents_pc__p}>
+        Примеры подходящих конвертов на{" "}
+        <a
+          href="https://www.amazon.it"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-main font-bold"
+        >
+          amazon.it
+        </a>
+      </p>
+      <Button
+        text="Узнать цену"
+        buttonType="filled"
+        margin="mt-4"
+        onClick={onClick}
+      />
+    </div>
+  );
+};
--- src/widgets/ui/HomePage/screens/CalculateScreen/CalculateScreen.tsx
+++ src/widgets/ui/HomePage/screens/CalculateScreen/CalculateScreen.tsx
@@ -4,26 +4,54 @@ import {
 } from "@features/Form/Calculate/calculate-form";
 import Button from "@shared/ui/Button/ui/button";
 import styles from "./styles.module.scss";
+import { useState } from "react";
+import { Documents } from "@shared/ui/Documents";
 export const CalculateScreen = () => {
+  const [isForm, setIsForm] = useState(true);
+  const handleToggleForm = () => {
+    setIsForm(!isForm);
+  };
   return (
     <>
       <section className={styles.calculate} id="calculate-mob">
         <h2 className={styles.calculate__heading}>
           Рассчитать стоимость доставки
         </h2>
-        <Button text="Посылка" buttonType="filled" margin="mt-6" />
-        <Button text="Документы" buttonType="outline" margin="mt-4" />
-        <CalculateForm />
+        <Button
+          text="Посылка"
+          buttonType={isForm ? "filled" : "outline"}
+          margin="mt-6"
+          onClick={handleToggleForm}
+        />
+        <Button
+          text="Документы"
+          buttonType={!isForm ? "filled" : "outline"}
+          margin="mt-4"
+          onClick={handleToggleForm}
+        />
+        {isForm && <CalculateForm />}
+        {!isForm && <Documents onClick={handleToggleForm} />}
       </section>
       <section className={styles.calculate_pc} id="calculate-pc">
         <h2 className={styles.calculate_pc__heading}>
           Рассчитать стоимость доставки
         </h2>
         <div className="w-full flex items-center justify-center gap-4 mt-4">
-          <Button text="Посылка" buttonType="filled" />
-          <Button text="Документы" buttonType="outline" />
+          <Button
+            text="Посылка"
+            buttonType={isForm ? "filled" : "outline"}
+            margin="mt-6"
+            onClick={handleToggleForm}
+          />
+          <Button
+            text="Документы"
+            buttonType={!isForm ? "filled" : "outline"}
+            margin="mt-4"
+            onClick={handleToggleForm}
+          />
         </div>
-        <CalculateFormPC />
+        {isForm && <CalculateFormPC />}
+        {!isForm && <Documents onClick={handleToggleForm} />}
       </section>
     </>
   );
