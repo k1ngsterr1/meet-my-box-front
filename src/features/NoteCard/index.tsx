762f53d952a396f3fc0ec128fbccc87d75f908a5
Erlan-bam <erlanzh.gg@gmail.com>
2024-09-11 19:28:34 +0500

    --NOTE FINISHED

---
+++ src/features/NoteCard/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./styles.module.scss";
+import Button from "@shared/ui/Button/ui/button";
+import { BorderInput } from "@shared/ui/Input/BorderInput/border-input";
+export const NoteCard = ({ onNoteClick, setter }: any) => {
+  const handleClick = () => {
+    onNoteClick();
+  };
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.heading}>Примечание</h3>
+      <input
+        placeholder="Длина"
+        onChange={(e) => setter(e.target.value)}
+        className={styles.input}
+      />
+      <Button
+        text="Далее"
+        buttonType="filled"
+        onClick={handleClick}
+        margin="mt-4"
+      />
+    </div>
+  );
+};
--- src/widgets/ui/ApplicationPage/index.tsx
+++ src/widgets/ui/ApplicationPage/index.tsx
@@ -1,12 +1,15 @@
 import { CourierCard } from "@features/Cards/CourierCard";
 import { InsuranceCard } from "@features/Cards/InsuranceCard";
+import { NoteCard } from "@features/NoteCard";
 import { useState } from "react";
 
 export const ApplicationPage = () => {
   const [showInsuranceCard, setShowInsuranceCard] = useState(true);
   const [showCourierCard, setShowCourierCard] = useState(false);
+  const [showNoteCard, setShowNoteCard] = useState(false);
   const [insurance, setInsurance] = useState(false);
   const [caurier, setCourier] = useState(false);
+  const [note, setNote] = useState("");
   const handleInsuranceClick = (value: boolean) => {
     setInsurance(value);
     setShowInsuranceCard(false);
@@ -15,6 +18,11 @@ export const ApplicationPage = () => {
   const handleCourierClick = (value: boolean) => {
     setCourier(value);
     setShowCourierCard(false);
+    setShowNoteCard(true);
+  };
+  const handleNoteClick = (value: string) => {
+    setShowNoteCard(false);
+    console.log("setting note", note);
   };
   return (
     <div className="w-full flex items-center justify-center">
@@ -22,6 +30,9 @@ export const ApplicationPage = () => {
         <InsuranceCard onInsuranceClick={handleInsuranceClick} />
       )}
       {showCourierCard && <CourierCard onCourierClick={handleCourierClick} />}
+      {showNoteCard && (
+        <NoteCard onNoteClick={handleNoteClick} setter={setNote} />
+      )}
     </div>
   );
 };
