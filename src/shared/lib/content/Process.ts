e1e2e6f8cf29a04a8fe35fb12dc9b1992c23720f
Erlan-Bam <erlanzh.gg@gmail.com>
2024-09-15 21:37:57 +0500

    --MERGED

--- src/shared/lib/content/Process.ts
+++ src/shared/lib/content/Process.ts
@@ -1,4 +1,37 @@
+import img3 from "@assets/boxes.webp";
+import img2 from "@assets/laptop.webp";
+import img1 from "@assets/process_1.png";
+import img4 from "@assets/process_4.png";
+import img5 from "@assets/service2.png";
 export interface IImageCardItem {
-  img: string;
+  img: ImageMetadata;
   description: string;
 }
+export const processList: IImageCardItem[] = [
+  {
+    img: img1,
+    description: "Подготовить посылку самостоятельно: измерить, взвесить",
+  },
+  {
+    img: img2,
+    description: "Оформить заказ на нашем сайте https://meetmybox.com",
+  },
+  {
+    img: img3,
+    description: "Распечатать и наклеить этикетки",
+  },
+  {
+    img: img4,
+    description: "Передать курьеру (посылочка сразу поедет к получателю)",
+  },
+];
+export const serviceList: IImageCardItem[] = [
+  {
+    img: img2,
+    description: "Cервис доставки посылок из Европы в Россию и страны СНГ",
+  },
+  {
+    img: img5,
+    description: "Покупки в интернет-магазина Европы",
+  },
+];
