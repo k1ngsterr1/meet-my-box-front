3a2d3bd060e37f487eb43ba2858bde8fa5ee1165
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-09-17 15:08:34 +0600

    --FIXED SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  // output: "client",
   integrations: [react(), tailwind()],
   vite: {
     resolve: {
