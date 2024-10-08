a281a1a57dc4043f47510b42f1871d1521f5255d
Erlan-Bam <erlanzh.gg@gmail.com>
2024-10-08 20:42:17 +0500

    --UPDATED

--- src/entities/AdditionalServiceTable/index.tsx
+++ src/entities/AdditionalServiceTable/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.scss";
 
-const PricingTable = () => {
+const PricingTable = ({ help }: any) => {
   return (
     <div className="w-full flex justify-center mt-4">
       <table
@@ -8,50 +8,19 @@ const PricingTable = () => {
       >
         <thead className={styles.table__head}>
           <tr className="bg-blue-600 text-white text-xs">
-            <th className="py-2 px-4 text-left">Наименование услуги</th>
-            <th className="py-2 px-4 text-left"></th>
-            <th className="py-2 px-4 text-left">Стоимость (EUR)</th>
+            <th className="py-2 px-4 text-left">{help.help.heading1}</th>
+            <th className="py-2 px-4 text-left">{help.help.heading2}</th>
+            <th className="py-2 px-4 text-left">{help.help.heading3}</th>
           </tr>
         </thead>
         <tbody className={styles.table__body}>
-          <tr className="border-t hover:bg-gray-50">
-            <td className="py-2 px-4 text-gray-800 text-sm">
-              Выкуп товара в интернет-магазине (включает в себя все услуги по
-              заказу)
-            </td>
-            <td className="py-2 px-4 text-gray-800 text-sm">-</td>
-            <td className="py-2 px-4 text-gray-800 text-sm">
-              Стоимость по запросу
-            </td>
-          </tr>
-          <tr className="border-t hover:bg-gray-50">
-            <td className="py-2 px-4 text-gray-800 text-sm">
-              Прием и переупаковка заказа
-            </td>
-            <td className="py-2 px-4 text-gray-800 text-sm">1 кг</td>
-            <td className="py-2 px-4 text-gray-800 text-sm">10€</td>
-          </tr>
-          <tr className="border-t hover:bg-gray-50">
-            <td className="py-2 px-4 text-gray-800 text-sm">
-              Проверка на соответствие
-            </td>
-            <td className="py-2 px-4 text-gray-800 text-sm">1 шт</td>
-            <td className="py-2 px-4 text-gray-800 text-sm">5€</td>
-          </tr>
-          <tr className="border-t hover:bg-gray-50">
-            <td className="py-2 px-4 text-gray-800 text-sm">
-              Консолидация/разделение заказов по разным посылкам
-            </td>
-            <td className="py-2 px-4 text-gray-800 text-sm">1 кг</td>
-            <td className="py-2 px-4 text-gray-800 text-sm">5€</td>
-          </tr>
-          <tr className="border-t hover:bg-gray-50">
-            <td className="py-2 px-4 text-gray-800 text-sm">
-              Возврат заказа, ведение переговоров с магазином
-            </td>
-            <td className="py-2 px-4 text-gray-800 text-sm">1 шт</td>
-            <td className="py-2 px-4 text-gray-800 text-sm">15€</td>
-          </tr>
+          {help.values.map((item: any) => (
+            <tr key={item.id} className="border-t hover:bg-gray-50">
+              <td className="py-2 px-4 text-gray-800 text-sm">{item.value1}</td>
+              <td className="py-2 px-4 text-gray-800 text-sm">{item.value2}</td>
+              <td className="py-2 px-4 text-gray-800 text-sm">{item.value3}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
